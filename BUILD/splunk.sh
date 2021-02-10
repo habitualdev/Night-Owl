806bafaca4ffe13e87aa8902a386e02bcfe186c4
Corbett Ferguson <habitual@habitual-system.habitual-media.com>
2021-02-10 07:02:05 -0500

    Zeek work

--- BUILD/splunk.sh
+++ BUILD/splunk.sh
@@ -5,3 +5,13 @@ wget -O splunkforwarder-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadAct
 apt install ./splunkforwarder-amd64.deb
 
 export PATH="/opt/splunkforwarder/bin:$PATH"
+
+read master -p "Splunk host IP Address: "
+
+read port -p "Splunk forwarder port: "
+
+/opt/splunkforwarder/bin/splunk add forward-server $master:$port --accept-license
+
+/opt/splunkforwarder/bin/splunk add monitor /var/log/snort
+
+/opt/splunkforwarder/bin/splunk add monitor /opt/zeek/
--- BUILD/zeek-config.sh
+++ BUILD/zeek-config.sh
@@ -1,6 +1,8 @@
 #Configure zeek with all the required home information
 
 
+ipaddr=$(ip address | grep "scope global" | cut -d " " -f 6 | cut -d "/" -f 1)
+
 ####HOME NETWORKS####
 continue=0
 #just some really simple regex to prevent basic errors#
@@ -28,7 +30,10 @@ do
 	echo $cidr >> /opt/zeek/etc/networks.cfg
 done
 
+echo "type=standalone" > /opt/zeek/etc/node.cfg
+echo "host=$ipaddr" >> /opt/zeek/etc/node.cfg
+echo "interface=ens160" >> /opt/zeek/etc/node.cfg
 
-
+echo "@load policy/tuning/json=logs.zeek" >> /opt/zeek/share/zeek/site/local.zeek
 
 
