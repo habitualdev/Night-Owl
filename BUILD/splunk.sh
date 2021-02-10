6ec945e287c87d7967c84423378ffe2eb22c055c
Corbett Ferguson <habitual@habitual-system.habitual-media.com>
2021-02-09 21:43:03 -0500

    Added paths to PATH

---
+++ BUILD/splunk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -O splunkforwarder-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.2&product=universalforwarder&filename=splunkforwarder-8.1.2-545206cc9f70-linux-2.6-amd64.deb&wget=true'
+
+apt install ./splunkforwarder-amd64.deb
+
+export PATH="/opt/splunkforwarder/bin:$PATH"
