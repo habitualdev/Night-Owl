d6a377d384e2915b018a6c5d9a72b8893dcaa9f7
Corbett Ferguson <habitual@habitual-system.habitual-media.com>
2021-02-10 16:29:31 -0500

    y? -y

--- BUILD/build.sh
+++ BUILD/build.sh
@@ -5,7 +5,7 @@ apt install -y gpg curl
 echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
 curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
 sudo apt update
-sudo apt install zeek-lts
+sudo apt install -y zeek-lts
 export PATH=/opt/zeek/bin:$PATH
 
 #Store some variables to make life easier
