2553d91072306ccfba1bbd6772bc735212217317
Corbett Ferguson <habitual@habitual-system.habitual-media.com>
2021-02-16 21:11:30 -0500

    ubuntu/centos split

---
+++ BUILD/Ubuntu/snort-config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+pwd
+cp config/snort.lua /usr/local/etc/snort/snort.lua
+
+cp config/snort_defaults.lua /usr/local/etc/snort/snort_defaults.lua
+
+groupadd snort
+
+useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
+
+mkdir -p /var/log/snort
+
+chmod -R 5775 /var/log/snort
+
+chown -R snort:snort /var/log/snort
+
+cp config/snort3.service  /lib/systemd/system/snort3.service
+
+systemctl enable snort3
+
+service snort3 start
+
+./splunk.sh
---
+++ BUILD/Ubuntu/zeek-config.sh
@@ -0,0 +1,23 @@
+#Configure zeek with all the required home information
+
+echo "[zeek]" > /opt/zeek/etc/node.cfg
+echo "type=standalone" >> /opt/zeek/etc/node.cfg
+echo "host=localhost" >> /opt/zeek/etc/node.cfg
+echo "interface=ens160" >> /opt/zeek/etc/node.cfg
+
+#Set up splunk forwarding of logs
+echo "@load policy/tuning/json-logs.zeek" >> /opt/zeek/share/zeek/site/local.zeek
+
+echo "[thruput]" >> /opt/splunkforwarder/etc/system/local/limits.conf
+echo "maxKBps = 0 # means unlimited" >> /opt/splunkforwarder/etc/system/local/limits.conf
+
+#Define the inputs
+cat config/zeek-outputs.conf >> /opt/splunkforwarder/etc/system/local/inputs.conf
+
+#create and enable zeek service
+cp config/zeek.service  /lib/systemd/system/zeek.service
+systemctl enable zeek
+
+
+#begin logging
+/opt/splunkforwarder/bin/splunk start
