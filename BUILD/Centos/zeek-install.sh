059d5da721420b001ebdfd860cbf299f0b242f2d
roaldi <65696348+roaldi@users.noreply.github.com>
2021-02-18 15:27:51 -0500

    Update zeek-install.sh

--- BUILD/Centos/zeek-install.sh
+++ BUILD/Centos/zeek-install.sh
@@ -5,3 +5,4 @@ wget https://download.opensuse.org/repositories/security:zeek/CentOS_8/security:
 yum install zeek-lts
 echo 'export PATH="$PATH:/opt/zeek/bin"' >> /etc/profile
 cd $workdir
+./zeek-config.sh
